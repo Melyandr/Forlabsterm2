3a773bfd78f931f4dba25af244ebe6bec4c8202f
Melyandr <119205312+Melyandr@users.noreply.github.com>
2023-03-31 10:42:37 +0300

    added laba 2

---
+++ Pen.java
@@ -0,0 +1,26 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Getter
+@Setter
+
+
+public abstract class Pen {
+    private String id="isn-101";
+    private String brand;
+    private String colour;
+    private String material;
+    private int size;
+
+
+
+
+    abstract int calculatePrice();
+
+
+
+}
